1ec3c8c5f8cd69860399c438c59975d7b609d113
Odin <kammerloher.andreas@gmail.com>
2019-07-28 16:51:10 +0200

    post gdx-setup commit

---
+++ desktop/src/de/odin_matthias/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.odin_matthias.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.odin_matthias.KhessGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KhessGame(), config);
+	}
+}
