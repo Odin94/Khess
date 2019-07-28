161e2deca150b2f102e705a700080a5988cc5104
Odin <kammerloher.andreas@gmail.com>
2019-07-28 17:41:36 +0200

    Can't get loading assets to work :c

--- core/src/de/odin_matthias/khess/resources/TextureRepository.kt
+++ core/src/de/odin_matthias/khess/resources/TextureRepository.kt
@@ -1,12 +1,11 @@
 package de.odin_matthias.khess.resources
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 
 
 object TextureRepository {
-    const val pathPrefix = "resources"
+    const val pathPrefix = "data"
 
-    val BLACK_TILE = Texture(Gdx.files.internal("$pathPrefix/black_tile.png"))
-    val WHITE_TILE = Texture(Gdx.files.internal("$pathPrefix/white_tile.png"))
+    val BLACK_TILE = Texture("$pathPrefix/black_tile.png")
+    val WHITE_TILE = Texture("badlogic.jpg") //Texture("$pathPrefix/white_tile.png")
 }
--- desktop/src/de/odin_matthias/desktop/DesktopLauncher.java
+++ desktop/src/de/odin_matthias/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package de.odin_matthias.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import de.odin_matthias.KhessGame;
+import de.odin_matthias.khess.game.GameApplication;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new KhessGame(), config);
+		new LwjglApplication(new GameApplication(), config);
 	}
 }
