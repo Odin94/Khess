69976e6b82e8006725edc9e69774c7765cc083d1
Odin <kammerloher.andreas@gmail.com>
2019-07-28 18:56:40 +0200

    Add pieces to board

--- core/src/de/odin_matthias/khess/EntityFactory.kt
+++ core/src/de/odin_matthias/khess/EntityFactory.kt
@@ -4,23 +4,120 @@ import com.badlogic.ashley.core.Engine
 import de.odin_matthias.khess.components.PositionComponent
 import de.odin_matthias.khess.components.VisualComponent
 import de.odin_matthias.khess.game.GameConfig.TILE_SIZE
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_BISHOP
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_KING
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_KNIGHT
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_PAWN
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_QUEEN
+import de.odin_matthias.khess.resources.TextureRepository.BLACK_ROOK
 import de.odin_matthias.khess.resources.TextureRepository.BLACK_TILE
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_BISHOP
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_KING
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_KNIGHT
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_PAWN
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_QUEEN
+import de.odin_matthias.khess.resources.TextureRepository.WHITE_ROOK
 import de.odin_matthias.khess.resources.TextureRepository.WHITE_TILE
 import ktx.ashley.entity
 
+enum class PIECE_COLOR {
+    BLACK,
+    WHITE
+}
 
 object EntityFactory {
     fun addTile(engine: Engine, row: Float, col: Float) =
             engine.entity {
                 with<VisualComponent>() {
-                    texture = getTexture(row.toInt(), col.toInt())
+                    texture = if (row % 2 == col % 2) WHITE_TILE else BLACK_TILE
                 }
                 with<PositionComponent> {
                     x = row * TILE_SIZE
                     y = col * TILE_SIZE
                 }
             }
-}
 
-fun getTexture(row: Int, col: Int) =
-        if (row % 2 == col % 2) WHITE_TILE else BLACK_TILE
+    fun addPawn(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_PAWN
+                        PIECE_COLOR.WHITE -> WHITE_PAWN
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+
+    fun addRook(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_ROOK
+                        PIECE_COLOR.WHITE -> WHITE_ROOK
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+
+    fun addKnight(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_KNIGHT
+                        PIECE_COLOR.WHITE -> WHITE_KNIGHT
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+
+    fun addBishop(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_BISHOP
+                        PIECE_COLOR.WHITE -> WHITE_BISHOP
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+
+    fun addQueen(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_QUEEN
+                        PIECE_COLOR.WHITE -> WHITE_QUEEN
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+
+    fun addKing(engine: Engine, row: Float, col: Float, color: PIECE_COLOR) =
+            engine.entity {
+                with<VisualComponent>() {
+                    texture = when (color) {
+                        PIECE_COLOR.BLACK -> BLACK_KING
+                        PIECE_COLOR.WHITE -> WHITE_KING
+                    }
+                }
+                with<PositionComponent>() {
+                    x = row * TILE_SIZE
+                    y = col * TILE_SIZE
+                }
+            }
+}
--- core/src/de/odin_matthias/khess/board/Board.kt
+++ core/src/de/odin_matthias/khess/board/Board.kt
@@ -1,7 +1,14 @@
 package de.odin_matthias.khess.board
 
 import com.badlogic.ashley.core.Engine
+import de.odin_matthias.khess.EntityFactory.addBishop
+import de.odin_matthias.khess.EntityFactory.addKing
+import de.odin_matthias.khess.EntityFactory.addKnight
+import de.odin_matthias.khess.EntityFactory.addPawn
+import de.odin_matthias.khess.EntityFactory.addQueen
+import de.odin_matthias.khess.EntityFactory.addRook
 import de.odin_matthias.khess.EntityFactory.addTile
+import de.odin_matthias.khess.PIECE_COLOR
 import de.odin_matthias.khess.systems.RenderSystem
 
 
@@ -15,6 +22,8 @@ class Board(private val width: Int, private val height: Int) {
 
     private fun generateEntities() {
         generateTiles(width, height)
+        generateBlackPieces()
+        generateWhitePieces()
     }
 
     private fun generateSystems() {
@@ -27,5 +36,41 @@ class Board(private val width: Int, private val height: Int) {
                 addTile(engine, row.toFloat(), col.toFloat())
             }
         }
+    }
+
+    private fun generateBlackPieces() {
+        for (row in 0 until width) {
+            addPawn(engine, row.toFloat(), 1f, PIECE_COLOR.BLACK)
+        }
+
+        addRook(engine, 0f, 0f, PIECE_COLOR.BLACK)
+        addRook(engine, 7f, 0f, PIECE_COLOR.BLACK)
+
+        addKnight(engine, 1f, 0f, PIECE_COLOR.BLACK)
+        addKnight(engine, 6f, 0f, PIECE_COLOR.BLACK)
+
+        addBishop(engine, 2f, 0f, PIECE_COLOR.BLACK)
+        addBishop(engine, 5f, 0f, PIECE_COLOR.BLACK)
+
+        addQueen(engine, 3f, 0f, PIECE_COLOR.BLACK)
+        addKing(engine, 4f, 0f, PIECE_COLOR.BLACK)
+    }
+
+    private fun generateWhitePieces() {
+        for (row in 0 until width) {
+            addPawn(engine, row.toFloat(), 6f, PIECE_COLOR.WHITE)
+        }
+
+        addRook(engine, 0f, 7f, PIECE_COLOR.WHITE)
+        addRook(engine, 7f, 7f, PIECE_COLOR.WHITE)
+
+        addKnight(engine, 1f, 7f, PIECE_COLOR.WHITE)
+        addKnight(engine, 6f, 7f, PIECE_COLOR.WHITE)
+
+        addBishop(engine, 2f, 7f, PIECE_COLOR.WHITE)
+        addBishop(engine, 5f, 7f, PIECE_COLOR.WHITE)
+
+        addQueen(engine, 3f, 7f, PIECE_COLOR.WHITE)
+        addKing(engine, 4f, 7f, PIECE_COLOR.WHITE)
     }
 }
