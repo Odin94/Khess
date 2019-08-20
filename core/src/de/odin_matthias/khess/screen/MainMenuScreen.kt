96eddad03ac9e436eec7edfef59ffeb3b595fff8
Odin <kammerloher.andreas@gmail.com>
2019-08-20 16:57:59 +0200

    Add stage setup for main menu

--- core/src/de/odin_matthias/khess/screen/MainMenuScreen.kt
+++ core/src/de/odin_matthias/khess/screen/MainMenuScreen.kt
@@ -2,28 +2,66 @@ package de.odin_matthias.khess.screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
-import de.odin_matthias.khess.Camera
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.Stage
 import de.odin_matthias.khess.game.GameApplication
+import ktx.actors.stage
+import ktx.scene2d.Scene2DSkin
+import ktx.scene2d.label
+import ktx.scene2d.table
+import ktx.style.color
+import ktx.style.label
+import ktx.style.skin
 
 
-class MainMenuScreen(val game: GameApplication, private val camera: Camera = Camera()) : Screen {
+class MainMenuScreen(val game: GameApplication, private val stage: Stage = stage()) : Screen {
+
+    init {
+        stage.camera.position.set(0f, -Gdx.graphics.height.toFloat() / 4, 0f)
+        Gdx.input.inputProcessor = stage
+
+        Scene2DSkin.defaultSkin = skin {
+            color("white", 1f, 1f, 1f, 1f)
+            color("black", red = 0f, green = 0f, blue = 0f)
+            color("red", red = 1f, green = 0f, blue = 0f, alpha = 1f)
+            label {
+                font = BitmapFont().also { it.data.scale(2f) }
+                fontColor = Color.CORAL
+            }
+        }
+        stage.addActor(
+                table {
+                    table {
+                        label("Khess")
+                        it.spaceBottom(10f).row()
+                    }
+                    table {
+                        label("Click to start") {
+                            color = Color.WHITE
+                        }
+                    }
+                }
+        )
+    }
+
     override fun render(delta: Float) {
-
-        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
+        Gdx.gl.glClearColor(0.15F, 0.1F, 0.1F, 1F)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        camera.update()
-        game.batch.projectionMatrix = camera.combined
+        stage.draw()
 
-        game.batch.begin()
-        game.font.draw(game.batch, "Khess - Click to play!", 100f, 150f)
-        game.batch.end()
+        update(delta)
 
         if (Gdx.input.isTouched) {
             game.screen = GameScreen(game)
             dispose()
         }
+    }
+
+    private fun update(delta: Float) {
+        stage.act(delta)
     }
 
     override fun show() {
